b8f6c6db385ddc6a553c8af8195c5e085cbaa41f
Sujay Thakur <st670@cam.ac.uk>
2017-02-04 10:23:00 +0000

    Added tests for Tasks 1D, E, F

--- partia-flood-warning-sytem/test_geo.py
+++ partia-flood-warning-sytem/test_geo.py
@@ -3,27 +3,165 @@
 import pytest
 from floodsystem.geo import rivers_with_station
 from floodsystem.geo import rivers_by_station_number
+from floodsystem.geo import stations_by_river
 from floodsystem.stationdata import build_station_list
-from floodsystem.geo import dict_fordemo
+from floodsystem.station import MonitoringStation
 
 
 station_list = build_station_list()
-
 
 def test_rivers_with_station():
     """Test rivers_with_station()"""
     rivers = rivers_with_station(station_list)
     assert len(rivers) > 0
 
+    # Create test station 1
+    s_id1 = "test-s-id1"
+    m_id1 = "test-m-id1"
+    label1 = "some station1"
+    coord1 = (-2.0, 4.0)
+    trange1 = (-2.3, 3.4445)
+    river1 = "River X1"
+    town1 = "My Town1"
+    s1 = MonitoringStation(s_id1, m_id1, label1, coord1, trange1, river1, town1)
+    
+    # Create test station 2
+    s_id2 = "test-s-id2"
+    m_id2 = "test-m-id2"
+    label2 = "some station2"
+    coord2 = (-2.0, 4.0)
+    trange2 = (-2.3, 3.4445)
+    river2 = None
+    town2 = "My Town2"
+    s2 = MonitoringStation(s_id2, m_id2, label2, coord2, trange2, river2, town2)
+    
+    test_list = [s1, s2]
+    rivers_test = rivers_with_station(test_list)
+    assert len(rivers_test) == 1
+    
+def test_stations_by_river():
+    """Test stations_by_river()"""
+    river_dict = stations_by_river(station_list)
+    assert len(river_dict) > 0
+
+    # Create test station 1
+    s_id1 = "test-s-id1"
+    m_id1 = "test-m-id1"
+    label1 = "some station1"
+    coord1 = (-2.0, 4.0)
+    trange1 = (-2.3, 3.4445)
+    river1 = "River X1"
+    town1 = "My Town1"
+    s1 = MonitoringStation(s_id1, m_id1, label1, coord1, trange1, river1, town1)
+    
+    # Create test station 2
+    s_id2 = "test-s-id2"
+    m_id2 = "test-m-id2"
+    label2 = "some station2"
+    coord2 = (-2.0, 4.0)
+    trange2 = (-2.3, 3.4445)
+    river2 = "River X1"
+    town2 = "My Town2"
+    s2 = MonitoringStation(s_id2, m_id2, label2, coord2, trange2, river2, town2)
+    
+    # Create test station 3
+    s_id3 = "test-s-id3"
+    m_id3 = "test-m-id3"
+    label3 = "some station3"
+    coord3 = (-2.0, 4.0)
+    trange3 = (-2.3, 3.4445)
+    river3 = "River X3"
+    town3 = "My Town3"
+    s3 = MonitoringStation(s_id3, m_id3, label3, coord3, trange3, river3, town3)
+    
+    
+    test_list = [s1, s2, s3]
+    river_dict_test = stations_by_river(test_list)
+    assert len(river_dict_test) == 2
+
 def test_rivers_by_station_number():
     """Test rivers_by_station_number()"""
-    N=9
-    first_N = rivers_by_station_number(dict_fordemo, N)
+    first_N = rivers_by_station_number(station_list, 9)
     assert len(first_N) > 0
-    for i in range (0, N):
-        if first_N[i][1] <= first_N[i+1][1]:
-            sort_checker = True
-        else:
-            sort_checker = False
-            
-    assert sort_checker == True
+
+    # Create test station 1
+    s_id1 = "test-s-id1"
+    m_id1 = "test-m-id1"
+    label1 = "some station1"
+    coord1 = (-2.0, 4.0)
+    trange1 = (-2.3, 3.4445)
+    river1 = "River X1"
+    town1 = "My Town1"
+    s1 = MonitoringStation(s_id1, m_id1, label1, coord1, trange1, river1, town1)
+    
+    # Create test station 2
+    s_id2 = "test-s-id2"
+    m_id2 = "test-m-id2"
+    label2 = "some station2"
+    coord2 = (-2.0, 4.0)
+    trange2 = (-2.3, 3.4445)
+    river2 = "River X1"
+    town2 = "My Town2"
+    s2 = MonitoringStation(s_id2, m_id2, label2, coord2, trange2, river2, town2)
+    
+    # Create test station 3
+    s_id3 = "test-s-id3"
+    m_id3 = "test-m-id3"
+    label3 = "some station3"
+    coord3 = (-2.0, 4.0)
+    trange3 = (-2.3, 3.4445)
+    river3 = "River X1"
+    town3 = "My Town3"
+    s3 = MonitoringStation(s_id3, m_id3, label3, coord3, trange3, river3, town3)
+    
+    # Create test station 4
+    s_id4 = "test-s-id4"
+    m_id4 = "test-m-id4"
+    label4 = "some station4"
+    coord4 = (-2.0, 4.0)
+    trange4 = (-2.3, 3.4445)
+    river4 = "River X2"
+    town4 = "My Town4"
+    s4 = MonitoringStation(s_id4, m_id4, label4, coord4, trange4, river4, town4)
+    
+    # Create test station 5
+    s_id5 = "test-s-id5"
+    m_id5 = "test-m-id5"
+    label5 = "some station5"
+    coord5 = (-2.0, 4.0)
+    trange5 = (-2.3, 3.4445)
+    river5 = "River X2"
+    town5 = "My Town5"
+    s5 = MonitoringStation(s_id5, m_id5, label5, coord5, trange5, river5, town5)
+    
+    # Create test station 6
+    s_id6 = "test-s-id6"
+    m_id6 = "test-m-id6"
+    label6 = "some station6"
+    coord6 = (-2.0, 4.0)
+    trange6 = (-2.3, 3.4445)
+    river6 = "River X4"
+    town6 = "My Town6"
+    s6 = MonitoringStation(s_id6, m_id6, label6, coord6, trange6, river6, town6)
+    
+    # Create test station 7
+    s_id7 = "test-s-id7"
+    m_id7 = "test-m-id7"
+    label7 = "some station7"
+    coord7 = (-2.0, 4.0)
+    trange7 = (-2.3, 3.4445)
+    river7 = "River X5"
+    town7 = "My Town7"
+    s7 = MonitoringStation(s_id7, m_id7, label7, coord7, trange7, river7, town7)
+    
+    test_list = [s1, s2, s3, s4, s5, s6, s7]
+
+    first_N_test_1 = rivers_by_station_number(test_list, 1)
+    first_N_test_2 = rivers_by_station_number(test_list, 2)
+    first_N_test_3 = rivers_by_station_number(test_list, 3)
+    
+    assert len(first_N_test_1) == 1
+    assert len(first_N_test_2) == 2
+    assert len(first_N_test_3) == 4
+
+
